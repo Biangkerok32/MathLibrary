bbe41f4795d59981f8ce6fbf781ede1e0f133775
Shubham Mahalkar <smahalkar@gmail.com>
2019-10-06 21:53:10 +0530

    Initial commit

---
+++ shubham_math/src/main/java/com/example/shubham_math/Math.java
@@ -0,0 +1,27 @@
+package com.example.shubham_math;
+
+/**
+ * Created by Shubham Mahalkar on 06-Oct-19
+ */
+public class Math
+{
+    public static int Plus(int a, int b)
+    {
+        return a+b;
+    }
+
+    public static int Minus(int a, int b)
+    {
+        return a-b;
+    }
+
+    public static int Multiple(int a, int b)
+    {
+        return a*b;
+    }
+
+    public static float Division(int a, int b)
+    {
+            return a/b;
+    }
+}
